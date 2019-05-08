a04e6620290e7bf629409aeb2953a37ebc132dac
Your Name <you@example.com>
2019-05-08 20:12:11 +0800

    2019.05.08

--- exp.py
+++ exp.py
@@ -1,25 +1,11 @@
-from bosonnlp import BosonNLP
+import pynlpir
 
-nlp = BosonNLP(token='jkk69hrE.34263.MwdMVTH2I0Zs')
+title = '仙桃“网红”景点走红的背后 “内功”还需提升'
 
+pynlpir.open()
 
-def print_cluster(docs, idx, result):
-    print('=' * 50)
-    print('第%d个聚类中共有%s份文档,如下:' % (idx + 1, result['num']))
-    for doc in result['list']:
-        print(docs[doc])
-    print('-' * 20)
-    print('本聚类的中心文档为:')
-    print(docs[result['_id']])
+r = pynlpir.segment(title)
 
+print(r)
 
-def main():
-    with open('data/NBA.txt', 'rb') as f:
-        docs = [line.decode('utf-8') for line in f if line]
-    clusters = nlp.cluster(docs)
-    clusters = sorted(clusters, key=lambda cluster: cluster['num'], reverse=True)
-    for idx, cluster in enumerate(clusters):
-        print_cluster(docs, idx, cluster)
-
-
-main()
+pynlpir.close()
